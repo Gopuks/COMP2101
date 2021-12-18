684b4a07e960d6a05a2256dea854714c10b5edb2
Gopuks <gopuu4321@gmail.com>
2021-12-17 21:18:02 -0500

    Create lab 5.ps1

---
+++ powershell/lab 5.ps1
@@ -0,0 +1,16 @@
+﻿param(
+      [switch]$System,
+      [switch]$Disks, 
+      [switch]$Network
+  )
+      if ($System -eq $true) {
+       OSinfo
+       MEMinfo
+       GPU
+    }
+      if ($Disks -eq $true) {
+        DISKinfo
+    }
+      if ($Network -eq $true) {
+        NetInfo
+    }
